d64d438333c79aaaf28e3bafff4f1373d4d93063
Sean Thorpe <sean.thorpe@west-mec.org>
2015-10-29 15:12:26 -0700

    Final Project

--- script.js
+++ script.js
@@ -6,10 +6,22 @@ function addFoods(){
     var purchaseDate = $('#purchasedate').val();
     var expirationDate = $('#expirationdate').val();
 
-    $('#savedFoods').append('<tr><td>' + foodName + '</td><td>' + definition + '</td><td>' + purchaseDate + '</td><td>' + expirationDate + '</td></tr>');
 
     $('#foodname').val('');
     $('#definition').val('');
     $('#purchasedate').val('');
     $('#expirationdate').val('');
+
+    var unixExpiration = Date.parse(expirationDate);
+    var today = new Date();
+    var unixToday = Date.parse(today);
+
+    if(unixToday > unixExpiration - 259200000){
+        $('#savedFoods').append('<tr style="background-color: red"><td>' + foodName + '</td><td>' + definition + '</td><td>' + purchaseDate + '</td><td>' + expirationDate + '</td></tr>');
+    }
+    else {
+        $('#savedFoods').append('<tr><td>' + foodName + '</td><td>' + definition + '</td><td>' + purchaseDate + '</td><td>' + expirationDate + '</td></tr>');
+    }
 }
+
+
